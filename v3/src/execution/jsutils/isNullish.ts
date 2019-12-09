b2f806bd650eb5d09a73f892cddc469ba5521d51
Trevor Scheer <trevor.scheer@gmail.com>
2019-12-09 13:49:02 -0800

    Flow -> TS: isNullish predicate function

--- v3/src/execution/jsutils/isNullish.ts
+++ v3/src/execution/jsutils/isNullish.ts
@@ -1,6 +1,8 @@
 /**
  * Returns true if a value is null, undefined, or NaN.
+ *
+ * Note: TS doesn't capture NaN as a type, but this is functional at runtime.
  */
-export default function isNullish(value: unknown): boolean %checks {
+export default function isNullish(value: unknown): value is null | undefined {
   return value === null || value === undefined || value !== value;
 }
