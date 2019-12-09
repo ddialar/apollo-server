3b8a3a293a6d76007323468583255eafce931dae
Trevor Scheer <trevor.scheer@gmail.com>
2019-12-09 13:48:38 -0800

    Flow -> TS: isInvalid predicate function

--- v3/src/execution/jsutils/isInvalid.ts
+++ v3/src/execution/jsutils/isInvalid.ts
@@ -1,6 +1,8 @@
 /**
  * Returns true if a value is undefined, or NaN.
+ *
+ * Note: TS doesn't capture NaN as a type, but this is functional at runtime.
  */
-export default function isInvalid(value: unknown): boolean %checks {
+export default function isInvalid(value: unknown): value is undefined {
   return value === undefined || value !== value;
 }
