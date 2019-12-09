bf720fa1224ffef960185ad9ea764791b9139817
Trevor Scheer <trevor.scheer@gmail.com>
2019-12-09 13:56:48 -0800

    Flow -> TS: isPromise predicate function

--- v3/src/execution/jsutils/isPromise.ts
+++ v3/src/execution/jsutils/isPromise.ts
@@ -2,10 +2,22 @@
  * Returns true if the value acts like a Promise, i.e. has a "then" function,
  * otherwise returns false.
  */
-declare function isPromise(value: unknown): boolean %checks(value instanceof
-  Promise);
-
-// eslint-disable-next-line no-redeclare
-export default function isPromise(value) {
+export default function isPromise<T>(value: any): value is PromiseLike<T> {
   return value != null && typeof value.then === 'function';
 }
+
+// This is possibly the right way to do it using unknown, but honestly it's
+// probably overkill. TS struggles to accept the existence of object.then without
+// the isThenable predicate.
+//
+// export function isPromise<T>(
+//   maybePromise: unknown
+// ): maybePromise is PromiseLike<T> {
+//   return isThenable(maybePromise) && typeof maybePromise.then === "function";
+// }
+
+// function isThenable(
+//   object: unknown
+// ): object is { [key: string]: any; then: unknown } {
+//   return typeof object === "object" && object !== null && "then" in object;
+// }
