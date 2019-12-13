08a4f18563de0463118aecdbfe89a79a0037f328
Jesse Rosenberger <git@jro.cc>
2019-12-13 19:05:40 +0200

    Add LICENSE to root of v3 package and attribute `graphql-js` for execution.

--- v3/src/execution/execute.ts
+++ v3/src/execution/execute.ts
@@ -1,3 +1,10 @@
+/**
+ * Portions of code within this module are leveraging code copied from the
+ * `graphql-js` library and modified to suit the needs of Apollo Server. For
+ * details regarding the terms of these modifications and any relevant
+ * attributions for the existing code, see the LICENSE file, at the root of this
+ * package.
+ */
 import { forEach, isCollection } from 'iterall';
 import {
   inspect,
--- v3/src/execution/types.ts
+++ v3/src/execution/types.ts
@@ -1,3 +1,11 @@
+/**
+ * Portions of code within this module are leveraging code copied from the
+ * `graphql-js` library and modified to suit the needs of Apollo Server. For
+ * details regarding the terms of these modifications and any relevant
+ * attributions for the existing code, see the LICENSE file, at the root of this
+ * package.
+ */
+
 import {
   GraphQLSchema,
   VariableDefinitionNode,
